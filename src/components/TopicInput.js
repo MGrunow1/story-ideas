f37cbe87a167bfb232535967fde4169a998f9aed
MGrunow1 <100005756+MGrunow1@users.noreply.github.com>
2023-05-01 19:15:08 -0700

    feat(TopicInput): Add basic styling

--- src/components/TopicInput.js
+++ src/components/TopicInput.js
@@ -1,17 +1,23 @@
+import styles from './TopicInput.module.css';
 import LoadingSpinner from "./LoadingSpinner";
 
 export default function TopicInput() {
     const isLoading = true;//TODO: delete and replace with props
     return (
-        <>
-        <input type="text" aria-label="Enter topic here" placeholder="Enter topic here" />
+      <>
+        <input
+            type="text"
+            aria-label="Enter topic here"
+            placeholder="Enter topic here"
+            className={styles.input}
+        />
         {isLoading ? (
             <div>
                 <LoadingSpinner />
             </div>
         ) : (
-            <button>Submit</button>
+          <button className={styles.submit}>Submit</button>
         )}
-        </>
+      </>
     )
 }
