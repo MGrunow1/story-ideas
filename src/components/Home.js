a6b2dddd8e68b8fd98b4b73172d4c0b8bfa096e3
MGrunow1 <100005756+MGrunow1@users.noreply.github.com>
2023-05-01 19:59:14 -0700

    feat(App): Allow inputting a topic

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,17 @@
+import { useState } from "react";
 import IdeaOutput from "./IdeaOutput";
 import TopicInput from "./TopicInput";
 
 export default function Home() {
-    const ideaText = "";//TODO: replace with fetched output
+    const [ideaText, setIdeaText] = useState('');
+    const [topic, setTopic] = useState('');
     //
     return (
         <>
         <h1>Silly Story Ideas</h1>
         <p>Input a simple subject for a topic (like "vacuum cleaner" or "cow"), and get a simple idea for a story.</p>
         <p>(This project accesses OpenAI's language models, but is not affiliated with OpenAI).</p>
-        <TopicInput />
+        <TopicInput topic={topic} topicSetter={setTopic} outputSetter={setIdeaText} />
         {ideaText && (
             <IdeaOutput ideaText={ideaText}/>
           )}
